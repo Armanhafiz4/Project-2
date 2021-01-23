3e60ebb88cf645b37f3f9339ce53739744380bc4
Armanhafiz4 <ahafiz15@hotmail.com>
2021-01-23 14:27:32 +0000

    feature finished

--- Codes/service_4/application/routes.py
+++ Codes/service_4/application/routes.py
@@ -3,9 +3,9 @@ from flask import request, jsonify
 
 @app.route("/position", methods=["POST"])
 def get_position():
-    suited_positions = {"pace" : "winger", "defending" : "centreback", "shooting" : "striker"}
+    suited_positions = {"pace" : "winger", "defending" : "centreback", "crossing" : "fullback"}
     strength = request.get_json()["strength"]
-    unsuited_positions = { "passing" : "striker", "dribbling" : "centreback", "heading" : "winger"}
+    unsuited_positions = { "passing" : "striker", "dribbling" : "centreback", "reflexes" : "goalkeeper"}
     weakness = request.get_json()["weakness"]
     app.logger.info(strength)
     app.logger.info(weakness)
